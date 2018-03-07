22347baba74d1e8fc4de87ace9d81b6001f008b9
Herber <herber523@dailyview.tw>
2018-03-07 10:38:11 +0800

    open jmx remote & fix jmx service load config

---
+++ scylla/scylla-jmx-service.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source /etc/sysconfig/scylla-jmx
+
+exec /usr/lib/scylla/jmx/scylla-jmx SCYLLA_JMX_PORT SCYLLA_API_PORT SCYLLA_API_ADDR SCYLLA_JMX_ADDR SCYLLA_JMX_FILE SCYLLA_JMX_LOCAL SCYLLA_JMX_REMOTE SCYLLA_JMX_DEBUG
